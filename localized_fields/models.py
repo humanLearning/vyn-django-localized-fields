8e202105dfdd3957140dd205c0b865f12ef31fe3
Tim Wood <tim@humanlearning.com>
2023-05-04 12:42:16 +0100

    latest version of docformatter has different formatting

--- localized_fields/models.py
+++ localized_fields/models.py
@@ -7,10 +7,11 @@ class LocalizedModel(AtomicSlugRetryMixin, PostgresModel):
     """Turns a model into a model that contains LocalizedField's.
 
     For basic localisation functionality, it isn't needed to inherit
-    from LocalizedModel. However, for certain features, this is required.
+    from LocalizedModel. However, for certain features, this is
+    required.
 
-    It is definitely needed for :see:LocalizedUniqueSlugField, unless you
-    manually inherit from AtomicSlugRetryMixin.
+    It is definitely needed for :see:LocalizedUniqueSlugField, unless
+    you manually inherit from AtomicSlugRetryMixin.
     """
 
     class Meta:
